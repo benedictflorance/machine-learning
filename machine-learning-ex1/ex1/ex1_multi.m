a06f31d7e018831c5cfca9d7ccd87c26a2078ddd
Benedict Florance <benedictflorance.a@gmail.com>
2017-12-26 11:50:35 +0530

    Computed price of a given house

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -104,8 +104,10 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
-
+x0 = 1;
+x1 = (1650 - mu(1)) / sigma(1);
+x2 = (3 - mu(2)) / sigma(2);
+price = [x0 x1 x2] * theta;
 
 % ============================================================
 
@@ -149,8 +151,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
-
+price = [1 1650 3] * theta;
 
 % ============================================================
 
